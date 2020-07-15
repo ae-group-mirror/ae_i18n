754cd4f4907aa403f67bb5e06c07f0105a47f2f6
AndiEcker <aecker2@gmail.com>
2020-07-15 19:44:43 +0100

    V 0.0.2: first alpha (unfinished - WIP).

--- ae/i18n.py
+++ ae/i18n.py
@@ -3,21 +3,27 @@ Internationalization / Localization helpers
 ===========================================
 
 """
+import ast
 import locale
 import os
-from typing import Dict, Union, Optional, Any
+from typing import Dict, Union, Optional, Any, Sequence
 
-from ae.core import stack_variables                                                 # type: ignore
-
-
-__version__ = '0.0.1'
+from ae.core import stack_variables, try_eval  # type: ignore
 
 
-MSG_FILE_SUFFIX = 'i18n.msg'
+__version__ = '0.0.2'
 
-_DOMAIN_LANGUAGES: Dict[str, str] = dict()
-_LOADED_LANGUAGES: Dict[str, Dict[str, Union[str, Dict[str, str]]]] = dict()
-_LANGUAGE, _ENCODING = locale.getdefaultlocale()
+
+MsgType = Union[str, Dict[str, str]]
+LanguageMessages = Dict[str, MsgType]
+
+
+MSG_FILE_SUFFIX = 'i18n.msg'                                #: message text files containing
+
+DOMAIN_LANGUAGES: Dict[str, str] = dict()                   #: associate a language to each domain
+LOADED_LANGUAGES: Dict[str, LanguageMessages] = dict()      #: message text translations of all loaded languages
+
+LANGUAGE, ENCODING = locale.getdefaultlocale()
 
 
 def add_domain(domain: str, language: str):
@@ -26,83 +32,114 @@ def add_domain(domain: str, language: str):
     :param domain:      domain id, e.g. the id of an app or a user.
     :param language:    language to translate to for the passed domain.
     """
-    global _DOMAIN_LANGUAGES
-    _DOMAIN_LANGUAGES[domain] = language
+    global DOMAIN_LANGUAGES
+    DOMAIN_LANGUAGES[domain] = language
 
 
-def load_language_texts(language: str, domain: str = ''):
+def load_language_texts(language: str, domain: str = '', file_paths: Sequence[str] = ('loc', )):
     """ load translatable message texts for the given language and optional domain.
 
     :param language:    language to load.
     :param domain:      optional domain id, e.g. the id of an app or a user. if passed
                         then a message file with the domain name as prefix will be preferred.
+    :param file_paths:  tuple of location folder root paths to search for message text files.
     """
-    if domain not in _DOMAIN_LANGUAGES:
+    def file_to_dict(file_name: str):
+        """ check passed file name (file_name) and if exists then load, parse and compile file content into a dict. """
+        if os.path.exists(file_name):
+            with open(file_name) as file_handle:  # refactor with de.core.file_content into ae.core
+                file_content = file_handle.read()
+            if file_content:
+                lang_messages = ast.literal_eval(file_content)
+                if lang_messages:
+                    global LOADED_LANGUAGES
+                    if language not in LOADED_LANGUAGES:
+                        LOADED_LANGUAGES[language] = dict()
+                    LOADED_LANGUAGES[language].update(lang_messages)
+
+    for path in file_paths:
+        path = os.path.join(path, language)
+        file_to_dict(os.path.join(path, MSG_FILE_SUFFIX))
+        if domain:
+            file_to_dict(os.path.join(path, domain + '_' + MSG_FILE_SUFFIX))
+
+    if domain not in DOMAIN_LANGUAGES:
         add_domain(domain, language)
 
-    file_name = os.path.join('loc', language)
-    if os.path.exists(os.path.join(file_name, domain + '_' + MSG_FILE_SUFFIX)):
-        file_name = os.path.join(file_name, domain + '_' + MSG_FILE_SUFFIX)
-    else:
-        file_name = os.path.join(file_name, MSG_FILE_SUFFIX)
-    with open(file_name) as file_handle:        # refactor with de.core.file_content into ae.core
-        file_content = file_handle.read()
 
-    global _LOADED_LANGUAGES
-    _LOADED_LANGUAGES[language] = eval(file_content)
-
-
-def get_text(text: str, count: int = 1, domain: str = '') -> str:
+def get_text(text: str, count: Optional[int] = None, domain: str = '') -> str:
     """ translate passed text string into the current language.
 
     :param text:        text message to be translated.
-    :param count:       pass int value if the translated text changes on pluralization (default=1).
+    :param count:       pass int value if the translated text has variants for their pluralization.
+                        The count value will be converted into an amount/pluralize key by the
+                        function :func:`plural_key`.
     :param domain:      domain id, identifying a configured/registered language.
     :return:            translated text message or the value passed into :paramref:`~get_text.text`
                         if no translation text got found for the current language.
     """
-    lang = _DOMAIN_LANGUAGES.get(domain, _LANGUAGE)
-    if lang in _LOADED_LANGUAGES:
-        translations = _LOADED_LANGUAGES[lang]
+    lang = DOMAIN_LANGUAGES.get(domain, LANGUAGE)
+    if lang in LOADED_LANGUAGES:
+        translations = LOADED_LANGUAGES[lang]
         if text in translations:
             trans = translations[text]
-            if isinstance(trans, dict):
-                if count == 0:
-                    key = 'zero'
-                elif count == 1:
-                    key = 'one'
-                elif count > 1:
-                    key = 'many'
-                elif count < 0:
-                    key = 'negative'
-                else:
-                    key = 'any'
-                text = trans.get(key, text)
-            else:
+            if isinstance(trans, str):
                 text = trans
+            else:
+                key = plural_key(count) if count is not None else 'any'
+                text = trans.get(key, text)
     return text
 
 
 _ = get_text         #: alias of :func:`get_text`.
 
 
-def get_f_string(f_string: str, count: int = 1, domain: str = '',
-                 g_vars: Optional[Dict[str, Any]] = None, l_vars: Optional[Dict[str, Any]] = None
+def get_f_string(f_string: str, count: Optional[int] = None, domain: str = '',
+                 glo_vars: Optional[Dict[str, Any]] = None, loc_vars: Optional[Dict[str, Any]] = None
                  ) -> str:
     """ translate passed f-string into a f-string of the current language.
 
     :param f_string:    f-string to be translated and evaluated.
     :param count:       pass if the translated text changes on pluralization (see :func:`get_text`).
+                        If passed then the value of this argument will be provided/overwritten in the
+                        globals as a variable with the name `count`.
     :param domain:      domain id, identifying a configured/registered language.
-    :param g_vars:      global variables used in the conversion of the f-string expression to a string.
-    :param l_vars:      local variables used in the conversion of the f-string expression to a string.
+    :param glo_vars:    global variables used in the conversion of the f-string expression to a string.
+    :param loc_vars:    local variables used in the conversion of the f-string expression to a string.
     :return:            translated text message or the evaluated string result of the expression passed into
                         :paramref:`~get_text.f_string` if no translation text got found for the current language.
+                        All syntax errors and exceptions occurring in the conversion of the f-string will be
+                        ignored and the original or translated f_string value will be returned in these cases.
     """
     f_string = get_text(f_string, count=count, domain=domain)
-    if not g_vars and not l_vars:
-        g_vars, l_vars, _ = stack_variables(max_depth=1)
-    return eval(f"f'{f_string}'", g_vars, l_vars)
+
+    if not glo_vars and not loc_vars:
+        glo_vars, loc_vars, _ = stack_variables(min_depth=3, max_depth=3)
+
+    if count is not None:
+        assert isinstance(glo_vars, dict)       # mypy
+        glo_vars['count'] = count
+
+    return try_eval('f"' + f_string + '"', ignored_exceptions=(Exception, ), glo_vars=glo_vars, loc_vars=loc_vars) \
+        or f_string
 
 
 f_ = get_f_string       #: alias of :func:`get_f_string`.
+
+
+def plural_key(count: int) -> str:
+    """ convert number in count into a dict key for to access the correct plural form.
+
+    :param count:       number of items used in the current context.
+    :return:            dict key within the MsgType part of the translation data structure.
+    """
+    if count == 0:
+        key = 'zero'
+    elif count == 1:
+        key = 'one'
+    elif count > 1:
+        key = 'many'
+    else:
+        key = 'negative'
+
+    return key
--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -1,18 +1,205 @@
 """ ae.i18n unit tests. """
+import os
+
+import pytest
+
 # noinspection PyProtectedMember
-from ae.i18n import _, _ENCODING, _LANGUAGE, _LOADED_LANGUAGES
+from ae.i18n import (
+    DOMAIN_LANGUAGES, ENCODING, LANGUAGE, LOADED_LANGUAGES, MSG_FILE_SUFFIX,
+    _, f_, add_domain, load_language_texts
+)
 
 
-def test_default_locale():
-    assert _ENCODING
-    assert isinstance(_ENCODING, str)
-    assert _LANGUAGE
-    assert isinstance(_LANGUAGE, str)
+test_message_texts = ("test message 1", "test message 2", "pluralize-able")
+pluralize_keys = ('zero', 'one', 'many', 'negative', 'any')
 
 
-def test_loaded_lang_type():
-    assert isinstance(_LOADED_LANGUAGES, dict)
+@pytest.fixture
+def lang_file_es():
+    """ provide test message file for the language es_ES. """
+    lang = 'es_ES'
+    fp = os.path.join('tests', lang)
+    os.mkdir(fp)
+    fn = os.path.join(fp, MSG_FILE_SUFFIX)
+    with open(fn, 'w') as file_handle:
+        file_handle.write('{\n')
+        file_handle.write(',\n'.join(['"' + t + '": "t m ' + t[-1] + '"' for t in test_message_texts[:2]]))
+        file_handle.write(',\n"' + test_message_texts[2] + '": {')
+        file_handle.write(', '.join(['"' + t + '": "' + t[0] + '"' for t in pluralize_keys]) + '}\n')
+        file_handle.write('}\n')
+    yield lang
+    if os.path.exists(fn):      # check if file exists because some exception/error-check tests need to delete the file
+        os.remove(fn)
+    if os.path.exists(fp):
+        os.rmdir(fp)
 
 
-def test_func_def():
-    assert callable(_)
+glo_var = 'glo_var_val'
+
+
+class TestDeclarations:
+    def test_default_locale(self):
+        assert ENCODING
+        assert isinstance(ENCODING, str)
+        assert LANGUAGE
+        assert isinstance(LANGUAGE, str)
+
+    def test_loaded_lang_type(self):
+        assert isinstance(LOADED_LANGUAGES, dict)
+
+    def test_func_aliases(self):
+        assert callable(_)
+        assert callable(f_)
+
+
+class TestMissingTranslation:
+    def test_get_text(self):
+        assert _("tst_msg") == "tst_msg"
+
+    def test_f_string_locals(self):
+        loc_var = 'loc_var_val'
+        assert f_("{loc_var}") == loc_var
+
+    def test_f_string_globals(self):
+        assert f_("{glo_var}") == glo_var
+
+    def test_f_string(self):
+        loc_var = 'loc_var_val'
+        assert f_("{glo_var}{loc_var}") == glo_var + loc_var
+
+
+class TestLangLoading:
+    def test_load_language_texts_str(self, lang_file_es):
+        assert not DOMAIN_LANGUAGES
+        load_language_texts(lang_file_es, file_paths=('tests',))
+
+        assert lang_file_es in LOADED_LANGUAGES
+        assert isinstance(LOADED_LANGUAGES[lang_file_es], dict)
+        assert LOADED_LANGUAGES[lang_file_es][test_message_texts[0]] == 't m 1'
+        assert LOADED_LANGUAGES[lang_file_es][test_message_texts[1]] == 't m 2'
+
+    def test_load_languages_texts_plural(self, lang_file_es):
+        load_language_texts(lang_file_es, file_paths=('tests',))    # test re-load because already loaded by prev tests
+
+        assert isinstance(LOADED_LANGUAGES[lang_file_es][test_message_texts[2]], dict)
+        for t in pluralize_keys:
+            assert LOADED_LANGUAGES[lang_file_es][test_message_texts[2]][t] == t[0]
+
+
+class TestWithLoadedTranslations:
+    def test_get_text(self, lang_file_es):
+        load_language_texts(lang_file_es, file_paths=('tests',))    # load for all other test methods of this class
+
+        assert _("tst_msg") == "tst_msg"
+        assert _(test_message_texts[0]) == "t m " + test_message_texts[0][-1]
+        assert _(test_message_texts[1]) == "t m " + test_message_texts[1][-1]
+
+    def test_f_string_locals(self):
+        loc_var = 'loc_var_val'
+        assert f_("{loc_var}") == loc_var
+
+    def test_f_string_globals(self):
+        assert f_("{glo_var}") == glo_var
+
+    def test_f_string(self):
+        loc_var = 'loc_var_val'
+        assert f_("{glo_var}{loc_var}") == glo_var + loc_var
+
+    def test_get_text_pluralized(self):
+        assert _(test_message_texts[2]) == 'a'    # any
+
+
+class TestCount:
+    def test_get_text(self):
+        assert _("tst_msg", count=3) == "tst_msg"
+
+    def test_f_string_locals(self):
+        loc_var = 'loc_var_val'
+        assert f_("{loc_var}", count=4) == loc_var
+
+    def test_f_string_globals(self):
+        assert f_("{glo_var}", count=5) == glo_var
+
+    def test_f_string(self):
+        loc_var = 'loc_var_val'
+        count = 6
+        assert f_("{glo_var}{loc_var}{count}", count=count) == glo_var + loc_var + str(count)
+
+    def test_get_text_pluralized(self, lang_file_es):
+        assert _(test_message_texts[2], count=-1) == 'n'    # negative
+        assert _(test_message_texts[2], count=0) == "z"     # zero
+        assert _(test_message_texts[2], count=1) == "o"     # one
+        assert _(test_message_texts[2], count=2) == "m"     # many
+        assert _(test_message_texts[2], count=3) == "m"
+        assert _(test_message_texts[2], count=999) == "m"
+
+    def test_get_text_pluralized_without_count(self, lang_file_es):
+        assert _(test_message_texts[2]) == "a"      # any
+
+    def test_f_string_pluralized_without_count(self, lang_file_es):
+        assert f_(test_message_texts[2]) == "a"     # any
+
+
+class TestDomain:
+    def test_add_domain(self):
+        try:
+            add_domain('tst', 'de_DE')
+            assert DOMAIN_LANGUAGES['tst'] == 'de_DE'
+
+            domain_count = len(DOMAIN_LANGUAGES)
+            add_domain('tst', 'es_ES')
+            assert DOMAIN_LANGUAGES['tst'] == 'es_ES'
+            assert len(DOMAIN_LANGUAGES) == domain_count
+        finally:
+            DOMAIN_LANGUAGES.pop('tst')
+
+    def test_get_text(self, lang_file_es):
+        domain = 'tst_es_domain'
+        add_domain(domain, lang_file_es)
+        try:
+            load_language_texts(lang_file_es, domain=domain, file_paths=('tests',))
+            assert _("tst_msg", domain=domain) == "tst_msg"
+            assert _(test_message_texts[0], domain=domain) == "t m " + test_message_texts[0][-1]
+        finally:
+            DOMAIN_LANGUAGES.pop(domain)
+
+    def test_f_string_locals(self, lang_file_es):
+        domain = 'tst_es_domain'
+        add_domain(domain, lang_file_es)
+        try:
+            loc_var = 'loc_var_val'
+            assert f_("{loc_var}", domain=domain) == loc_var
+        finally:
+            DOMAIN_LANGUAGES.pop(domain)
+
+    def test_f_string_globals(self):
+        domain = 'tst_es_domain'
+        add_domain(domain, lang_file_es)
+        try:
+            assert f_("{glo_var}", domain=domain) == glo_var
+        finally:
+            DOMAIN_LANGUAGES.pop(domain)
+
+    def test_f_string(self, lang_file_es):
+        domain = 'tst_es_domain'
+        add_domain(domain, lang_file_es)
+        try:
+            loc_var = 'loc_var_val'
+            count = 6
+            assert f_("{glo_var}{loc_var}{count}", count=count, domain=domain) == glo_var + loc_var + str(count)
+        finally:
+            DOMAIN_LANGUAGES.pop(domain)
+
+    def test_get_text_pluralized(self, lang_file_es):
+        domain = 'tst_es_domain'
+        try:
+            load_language_texts(lang_file_es, domain=domain, file_paths=('tests',))  # reload with domain
+            assert _(test_message_texts[2], domain=domain) == "a"  # any
+            assert _(test_message_texts[2], count=-1, domain=domain) == "n"  # negative
+            assert _(test_message_texts[2], count=0, domain=domain) == "z"  # zero
+            assert _(test_message_texts[2], count=1, domain=domain) == "o"  # one
+            assert _(test_message_texts[2], count=2, domain=domain) == "m"  # many
+            assert _(test_message_texts[2], count=3, domain=domain) == "m"
+            assert _(test_message_texts[2], count=999, domain=domain) == "m"
+        finally:
+            DOMAIN_LANGUAGES.pop(domain)
